0c1d624f21cbdb3e18c9da3ee1e32f1e171e8a08
Jon Knapp <jon.knapp@rightscale.com>
2013-04-09 13:08:22 -0700

    added zsh completion for npm

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -48,3 +48,6 @@ zstyle ':completion:*:*:*:users' ignored-patterns \
 
 # ... unless we really want to.
 zstyle '*' single-ignored show
+
+# add other completion scripts
+fpath=("$(dirname $0)/completions" $fpath)
