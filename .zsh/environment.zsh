415eb54b0956c41f320208327e0018a2fa9fe58c
Jon Knapp <burtonboarder8@gmail.com>
2016-09-22 15:33:58 -0700

    Moved binaries to ~/.local/bin

--- .zsh/environment.zsh
+++ .zsh/environment.zsh
@@ -1,7 +1,7 @@
 #export EDITOR='vim -c start'
 export EDITOR='vim'
 export BROWSER='chromium'
-export PATH=~/.bin:$PATH
+export PATH=~/.local/bin:$PATH
 #export GREP_OPTIONS='-n --color=auto'
 export LESS='-R'
 export GTK2_RC_FILES="/etc/gtk-2.0/gtkrc:$HOME/.gtkrc-2.0"
